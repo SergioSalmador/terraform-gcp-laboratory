6557951feef6d68dc1af127cacd2db86291753a5
SergioSalmador <sergio.salmador@gmail.com>
2020-05-17 21:38:11 +0200

    add vpc-newtwork and subnetwork

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,25 +1,26 @@
 resource "google_compute_network" "vpc_network" {
-  name                    = "vpc-${var.}"
+  name                    = "vpc-${var.gcp_project_id}"
   auto_create_subnetworks = "false"
-  project                 = "${var.gcp_project_id}"
+  project                 = var.gcp_project_id
 }
 
-resource "random_id" "instance_id" {
-    byte_length = 8
-}
+#resource "random_id" "instance_id" {
+#    byte_length = 8
+#}
 
-resource "random_shuffle" "random_cidr" {
-    input = var.cidr
-}
+#resource "random_shuffle" "random_cidr" {
+#    input = var.cidr
+#}
 
 
-resource "random_shuffle" "random_region" {
-    input = var.region
-}
+#resource "random_shuffle" "random_region" {
+#    input = var.region
+#}
+
 resource "google_compute_subnetwork" "subnet-1" {
-    name = "subnet-${random_id.instance_id.hex}-var"
+    name = "subnet-${var.gcp_project_id}"
     network = "${google_compute_network.vpc_network.self_link}"
-    ip_cidr_range = "${random_shuffle.random_cidr.result[0]}"
-    region = "${random_shuffle.random_region.result[0]}"
-    project       = "${var.gcp_project_id}"
+    ip_cidr_range = var.ip_cidr
+    region = var.region
+    project = var.gcp_project_id
 }
---
+++ vpc/variables.tf
@@ -0,0 +1,14 @@
+variable "gcp_project_id" {
+  type    = string
+  default = "agile-aleph-273106"
+}
+
+variable "region" {
+  type    = string
+  default = "europe-west1"
+}
+
+variable "ip_cidr" {
+  type    = string
+  default = "10.20.0.0/24"
+}
